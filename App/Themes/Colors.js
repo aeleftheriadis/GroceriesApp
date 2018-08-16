6be7a63300b2ea9b10c2b4b94c7fae5f8eff1b20
Athanasios Eleftheriadis <athanasios.eleftheriadis@gmail.com>
2018-08-16 14:25:50 +0300

    Added packages and updated colors

--- App/Themes/Colors.js
+++ App/Themes/Colors.js
@@ -1,27 +1,24 @@
 const colors = {
-  background: '#1F0808',
+  background: '#E5E5E5',
   clear: 'rgba(0,0,0,0)',
-  facebook: '#3b5998',
   transparent: 'rgba(0,0,0,0)',
-  silver: '#F7F7F7',
-  steel: '#CCCCCC',
-  error: 'rgba(200, 0, 0, 0.8)',
-  ricePaper: 'rgba(255,255,255, 0.75)',
-  frost: '#D8D8D8',
-  cloud: 'rgba(200,200,200, 0.35)',
-  windowTint: 'rgba(0, 0, 0, 0.4)',
-  panther: '#161616',
-  charcoal: '#595959',
   coal: '#2d2d2d',
-  bloodOrange: '#fb5f26',
   snow: 'white',
   ember: 'rgba(164, 0, 48, 0.5)',
+  border: '#EAEDF1',
+  text: '#E0D7E5',
+  green: '#84AB56',
+  white: '#FFFFFF',
+  black: '#000000',
+  grey: '#A1A1A1',
+  darkGrey: '#767676',
+  imageBackground: '#777676',
+  textBorder: '#D4D4D4',
+  // TODO Remove after that line
+  steel: '#CCCCCC',
   fire: '#e73536',
-  drawer: 'rgba(30, 30, 29, 0.95)',
-  eggplant: '#251a34',
-  border: '#483F53',
-  banner: '#5F3E63',
-  text: '#E0D7E5'
+  bloodOrange: '#fb5f26'
+
 }
 
 export default colors
